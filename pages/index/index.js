153d10c48e1b58aaba55f3acc40e8276307f923b
chenwei <cryptic_v@163.com>
2017-12-13 19:33:30 +0800

    小程序自定义组件

---
+++ pages/index/index.js
@@ -0,0 +1,30 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+
+  /**
+ * 生命周期函数--监听页面初次渲染完成
+ */
+  onReady: function () {
+    //获得dialog组件
+    this.dialog = this.selectComponent("#dialog");
+  },
+
+  showDialog() {
+    this.dialog.showDialog();
+  },
+
+  //取消事件
+  _cancelEvent() {
+    console.log('你点击了取消');
+    this.dialog.hideDialog();
+  },
+  //确认事件
+  _confirmEvent() {
+    console.log('你点击了确定');
+    this.dialog.hideDialog();
+  }
+
+})
